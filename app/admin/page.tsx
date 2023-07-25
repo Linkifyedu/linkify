5f3b6414723dadf15fc4abd90ff69e52cb8ea298
eddyramirez <eduardo@linkify.lol>
2023-07-25 02:46:33 -0400

    fixed subjectcards

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,11 +1,14 @@
+import { Sidebar } from "@/components/sidebar";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
 
-export default function Home(){
+export default async function Home(){
     const session = await getServerSession(authOptions)
     if (session?.user.role === "admin"){
         return (
-
+            <Sidebar>
+                
+            </Sidebar>
             );
     } 
 
